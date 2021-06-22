7e30100bd42de2fc7826356c5254c454961c3b78
Alianhakim <alianhakim8@gmail.com>
2021-06-22 14:55:02 +0700

    'update'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+// namespace App\Models\User;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
